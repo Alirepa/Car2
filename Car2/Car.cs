3b0b60c9996ce673a3ee259ef26ed423a93edeb5
Ali <panahi.ali0606@gmail.com>
2025-09-18 13:04:36 +0300

    • Car class: Represents a car with properties like Brand, Color, MaxSpeed, HorsePower, and CarType. It includes methods such as ShowInfo(), Drive(), Honk(), Fuel(), and IsFasterThan(Car other) for displaying information, simulating driving, honking, fueling, and comparing speed. • CarType enum: Defines types of cars, such as Gas and Hybrid. • Program.cs: In the Main method, two Car objects (ferrari and volvo) are created and initialized with different values. Their methods are called to demonstrate their behavior and compare their speeds

---
+++ Car2/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car2
+{
+    internal class Car
+    {
+        public string Brand;
+        public string Color;
+        public int MaxSpeed;
+        public int HorsePower;
+        public CarType CarType;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Car Brand: {Brand}, Color: {Color}, Max Speed: {MaxSpeed} with horsepower of: {HorsePower} and the type of fuel is {CarType} ");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"The {Brand} is driving at {MaxSpeed}");
+        }
+
+        public void Honk() 
+        { 
+            Console.WriteLine("Honk Honk! ");
+        }
+
+        public void Fuel ()
+        {
+            Console.WriteLine($"Car is of type: {CarType}");
+            switch (CarType) 
+            {
+                case CarType.Electric: 
+                    Console.WriteLine("electric fuel");
+                    break;
+                case CarType.Gas:
+                    Console.WriteLine("Gas fuel");
+                    break;
+                case CarType.Disel:
+                    Console.WriteLine("Diesel fuel");
+                    break;
+                 case CarType.Hybrid:
+                    Console.WriteLine("Hybrid fuel");
+                    break;
+            }
+
+        }
+
+        public void IsFasterThan (Car otherCar)
+        {
+            if (this.MaxSpeed > otherCar.MaxSpeed)
+            {
+                Console.WriteLine($"{this.Brand} is faster than {otherCar.Brand}");
+            }
+            else if (this.MaxSpeed < otherCar.MaxSpeed)
+            {
+                Console.WriteLine($"{otherCar.Brand} is faster than {this.Brand}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Brand} and {otherCar.Brand} have the same speed");
+            }
+        }
+    }
+
+    
+}
--- Car2/Program.cs
+++ Car2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car ferrari = new Car();
+            ferrari.Brand = "Ferrari";
+            ferrari.Color = "Red";
+            ferrari.MaxSpeed = 350;
+            ferrari.HorsePower = 600;
+            ferrari.CarType = CarType.Gas;
+
+
+            Car volvo = new Car();
+            volvo.Brand = "Volvo";
+            volvo.Color = "Black";
+            volvo.MaxSpeed = 250;
+            volvo.HorsePower = 300;
+            volvo.CarType = CarType.Hybrid;
+
+            ferrari.ShowInfo();
+            ferrari.Drive();
+            ferrari.Honk();
+            ferrari.Fuel();
+
+            volvo.ShowInfo();
+            volvo.Drive();
+            volvo.Honk();
+            volvo.Fuel();
+
+            volvo.IsFasterThan(ferrari);
+
+
         }
     }
 }
